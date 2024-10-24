1412792f34fdd39eda977721f1bc7d051397b7f1
Joris Dh <joris.dh@outlook.be>
2024-10-24 15:49:01 +0200

    project setup with Maven and connection to CSV-file

---
+++ src/main/java/joris/Main.java
@@ -0,0 +1,35 @@
+package joris;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        String file = "src//covid_and_trade.csv";
+        BufferedReader reader = null;
+        String line = "";
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            while ((line = reader.readLine()) != null) {
+
+                String[] row = line.split(",");
+
+                for (String index : row) {
+                    System.out.printf("%-10s", index);
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
